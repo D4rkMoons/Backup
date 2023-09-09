eefb85f35e708c42c5d42f980010499a72e3da78
brissonsean <77543961+brissonsean@users.noreply.github.com>
2023-09-09 18:57:07 -0400

    Artix
    /

---
+++ Linux/nix/Arch/WM_OpenBox_arm64_armhf-aarch64.sh
@@ -0,0 +1,2 @@
+#!/data/data/com.termux/files/bin/bash
+pkg update -y && pkg install curl proot tar -y && curl https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Arch/armhf/arch-openbox.sh | bash
