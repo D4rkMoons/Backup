13f33a887f19e5ca55e3a5d06ae4e642122de120
D4rkMoons <77543961+D4rkMoons@users.noreply.github.com>
2023-09-21 22:25:32 -0400

    Delete Linux/nix/Arch directory

--- Linux/nix/Arch/xfce_arm64_armhf-aarch64.sh
+++
@@ -1,2 +0,0 @@
-#!/data/data/com.termux/files/bin/bash
-pkg update -y && pkg install curl proot tar -y && curl https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Arch/armhf/arch-xfce.sh | bash
